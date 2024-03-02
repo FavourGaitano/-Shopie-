2800818ec721cbf526662d23900b909d56b6f500
jacksonmwadia <mwadiajackson@gmail.com>
2024-03-02 15:28:51 +0800

    user dashboard

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
+import { PageNotfoundComponent } from './components/page-notfound/page-notfound.component';
 
 export const routes: Routes = [
 
@@ -11,8 +13,8 @@ export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'registration', component: RegisterComponent},
     { path: 'login', component: LoginComponent},
-    // { path: 'user', component: UserDashboardComponent},
+    { path: 'user', component: UserComponent},
     { path: 'home', component: HomeComponent},
-    // {path:'**', component: NotfoundComponent}
+    // {path:'**', component: PageNotfoundComponent},
     { path: 'cart', component: ShoppingCartComponent},
 ];
