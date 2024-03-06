16a3947392852d572c6028fe46394f56cd43384a
FavourGaitano <favourbuyanzi@gmail.com>
2024-03-06 22:23:36 +0300

    remove produc from cart

--- angular-frontend/src/app/services/cart.service.ts
+++ angular-frontend/src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { OneUsercartResponse } from '../interfaces/cartResponse.interface';
+import{deleteItemCart} from '../interfaces/cart.interface'
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,20 @@ export class CartService {
         token
       })
     })
+  }
+
+
+  deleteItemCart(id: string, details: deleteItemCart) {
+    const token = localStorage.getItem('authToken') as string;
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      token,
+    });
+
+
+    const req = new HttpRequest('DELETE', `http://localhost:4001/cart/${id}`, details, { headers });
+
+
+    return this.http.request(req);
   }
 }
